4203aeebeb8fecf5960bc129f02358dddb9f634a
techsrimani <techsrimani@gmail.com>
2020-11-20 09:43:56 +0530

    second commit

---
+++ src/components/contactusform/contact-us-form.component.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class ContactUsForm extends React.Component{
+    constructor(props) {
+        super(props);
+
+      }
+
+      render(){
+          return(
+              <div>
+          <div className="form-container">
+              form goes here
+
+
+          </div>
+          
+       
+  
+   
+   
+    </div>
+          );
+      }
+}
+
+export default ContactUsForm;
